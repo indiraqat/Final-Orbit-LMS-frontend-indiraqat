c4553354240a2d6dd086dd9dd2ee6cdb3516b848
indiraqat <indiraqatrunnada@gmail.com>
2026-09-09 16:49:06 +0700

    Remove backend from frontend repo, add gitignore

--- orbit-lms-backend/src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Orbit LMS API running at http://localhost:${PORT}`);
-});
